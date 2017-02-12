dc7c09ee2917466875327110fdf5b7973f398a1d
Mark Brouch <mbrouch@walmartlabs.com>
2017-02-12 15:01:08 -0600

    🔧 Initialize project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const path = require('path');
+const externals = require('webpack-node-externals');
+const pkg = require('./package.json');
+
+module.exports = {
+  entry: path.resolve(__dirname, pkg.main),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+    filename: path.basename(pkg.main)
+  },
+  target: 'node',
+  externals: [externals({
+    whitelist: ['engine-blackjack']
+  })],
+  module: {
+    loaders: [
+      { test:/\.js$/, loader:'babel' },
+      { test:/\.json$/, loader:'json' }
+    ]
+  }
+};
